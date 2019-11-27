bc643cee08dbe381d50b7ab4b0ca434fe49d3812
Sudheer Ghanathe <sudheerghanathe@yahoo.com>
2019-11-27 16:45:56 -0700

    correct ios

--- ios/Pods/Target Support Files/Pods-RNSQLitePerformance-tvOSTests/Pods-RNSQLitePerformance-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RNSQLitePerformance_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_RNSQLitePerformance_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-RNSQLitePerformance/Pods-RNSQLitePerformance-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RNSQLitePerformance : NSObject
-@end
-@implementation PodsDummy_Pods_RNSQLitePerformance
-@end
